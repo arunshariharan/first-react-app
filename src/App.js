6c338f8366ac0d85955ced1b31c4568e5568f8cc
Arun <arunshariharan@gmail.com>
2020-06-15 21:53:04 +1000

    Section 3, pass function with params as attrobutes to stateless components

--- src/App.js
+++ src/App.js
@@ -11,24 +11,41 @@ class App extends Component {
    ],
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     this.setState({
       persons: [
-        {name: 'Arun', age: Math.round(Math.random() * 100)},
+        {name: newName, age: Math.round(Math.random() * 100)},
         {name: 'MaxBrenner', age: Math.round(Math.random() * 100)},
         {name: 'San Churros', age: Math.round(Math.random() * 100)}
       ]
     });
   }
 
+  // 2 ways to pass in method with values - onClick={this.switchNameHandler(this, 'somename')} - preferred way
+  // or create a custom attribute like - click={() => this.switchHandler('someName')}.
+  // During execution, compiler evaluates the empty function here
+  // and calls the original method to execute and returns that value. This is not the preferred way though.
+
   render() {
     return (
       <div className="App">
         <h1>Hello, welcome to my app!</h1>
-        <button onClick={this.switchNameHandler}>Switch name!</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Games + Sleeping</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={this.switchNameHandler.bind(this, 'Koko Black')}>Switch name!</button>
+
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={() => this.switchNameHandler('Vanilla custard')}>
+            My hobbies: Games + Sleeping
+        </Person>
+
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
   return (
     <div>
-      <h3>Hello, my name is {props.name}. My age is {props.age}!</h3>
+      <h3 onClick={props.click}>Hello, my name is {props.name}. My age is {props.age}!</h3>
       <p>{props.children}</p>
     </div>
   );
