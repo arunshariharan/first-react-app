5b4ae2ac1d2f7059482530a839f0783c7eac4d51
Arun Hariharan Sivasubramaniyan <arun.hariharan@xero.com>
2020-07-05 14:30:15 +1000

    [4.3, 55] cleaner if else code using the javascript way

--- src/App.js
+++ src/App.js
@@ -49,34 +49,39 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = (
+      <div>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}>
+        </Person>
+
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={() => this.switchNameHandler('Vanilla custard')}
+          changed={this.nameChangedHandler}>
+            My hobbies: Games + Sleeping
+        </Person>
+
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}>
+        </Person>
+      </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hello, welcome to my app!</h1>
         <button
           style={styling}
           onClick={this.showPersonsHandler}>Show People List!</button>
-        { this.state.showPersons ? 
-            <div>
-              <Person
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age}>
-              </Person>
-
-              <Person
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                click={() => this.switchNameHandler('Vanilla custard')}
-                changed={this.nameChangedHandler}>
-                  My hobbies: Games + Sleeping
-              </Person>
-
-              <Person
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age}>
-              </Person>
-            </div>
-          : null
-        }
+        {persons}
       </div>
     );
   }
