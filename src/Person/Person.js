7888f80a30e5e9d222eaa9ace0685144a0ddd319
Arun Hariharan Sivasubramaniyan <arun.hariharan@xero.com>
2020-07-15 21:09:39 +1000

    [5,74] use css modules on media queries and Person.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,29 +1,14 @@
 import React from 'react';
-import './Person.css';
-import styled from 'styled-components';
-
-const StyledDiv = styled.div`
-  width: 60%;
-  box-shadow: antiquewhite;
-  border: 1px solid #eee;
-  margin: 16px auto;
-  padding: 16px;
-  box-shadow: 0 2px 3px darkslateblue;
-  text-align: center;
-
-  @media (min-width: 500px) {
-    width: 450px;
-  }
-`;
+import cssClasses from './Person.css';
 
 const person = (props) => {
 
   return (
-    <StyledDiv>
+    <div className={cssClasses.Person}>
       <h3 onClick={props.click}>Hello, my name is {props.name}. My age is {props.age}!</h3>
       <p>{props.children}</p>
       <input type="text" value={props.name} onChange={props.changed} />
-    </StyledDiv>
+    </div>
   );
 }
 
